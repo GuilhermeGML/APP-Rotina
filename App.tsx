5562e7de80b467cdd4e5b9c61e7330e47d536a43
GuilhermeGML <guilherme.montes@unesp.br>
2025-09-15 16:17:19 -0300

    Deploy

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AppNavigator from "./scr/navigation/AppNavigator";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return <AppNavigator />;
